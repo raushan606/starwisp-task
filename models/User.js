64a518814735a5bfa10653774f8d3e38004bb08d
Raushan <raushankumar606@gmail.com>
2020-09-02 23:14:53 +0530

    auth middleware

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = User = mongoose.model("user", UserSchema);
